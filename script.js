2f71e78cdf284898c8779cfdf90abc9353b25929
Chris <chris@webdev14.local>
2025-11-07 13:04:08 -0500

    done

---
+++ script.js
@@ -0,0 +1,12 @@
+let drinkName = prompt("what is the drink name?");
+let size = prompt("what size is your drink");
+let icelevel = prompt("What ice level do you want?");
+let sugarlevel = prompt("How much sugar do you want")
+let topping = prompt("What topping do you want")
+
+document.getElementById("drinkName").innerHTML = drinkName;
+document.getElementById("size").innerHTML = size;
+document.getElementById("icelevel").innerHTML = icelevel;
+document.getElementById("sugarlevel").innerHTML = sugarlevel;
+document.getElementById("topping").innerHTML = topping;
+
